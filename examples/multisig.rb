33e2da814d008ee34b4d8618da5cb3dccc5c1165
Vivek Marakana <vivek.marakana@gmail.com>
2017-12-03 17:52:17 +0530

    update example on how to send multisig transaction

--- examples/multisig.rb
+++ examples/multisig.rb
@@ -1,7 +1,7 @@
 require "bundler/setup"
 require "iota"
 
-iota = IOTA::Client.new(provider: 'http://node.lukaseder.de:14265')
+iota = IOTA::Client.new(provider: 'http://localhost:14265')
 
 # First co-signer uses index 0 and security level 3
 digestOne = iota.multisig.getDigest('ABCDFG', 0, 3)
@@ -55,3 +55,15 @@ firstSignedBundle = iota.multisig.addSignature(initiatedBundle, finalAddress, io
 finalBundle = iota.multisig.addSignature(firstSignedBundle, finalAddress, iota.multisig.getKey('FDSAG', 0, 3))
 
 puts "IS VALID SIGNATURE: #{iota.utils.validateSignatures(finalBundle, finalAddress)}"
+
+# Send signed transction
+# Uncomment following lines if you want to send transaction
+# trytes = finalBundle.map do |tx|
+#   iota.utils.transactionTrytes(tx)
+# end
+
+# iota.api.sendTrytes(trytes.reverse, 10, 14) do |st1, transactions|
+#   if st1
+#     puts data
+#   end
+# end
