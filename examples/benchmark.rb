a6fb83285a74feb14cd009c14efb93cd98887c2e
Vivek Marakana <vivek.marakana@gmail.com>
2017-12-12 15:43:29 +0530

    removing extra examples

--- examples/benchmark.rb
+++
@@ -1,8 +0,0 @@
-require "bundler/setup"
-require "iota"
-
-iota = IOTA::Client.new(provider: 'http://172.104.164.117:14265')
-
-hashes = ["QLMNETEZDOYSBQLRPRJIZNRRDZ9EKY9LCOLNIDQEZNUFWOVYR9SJLBCVIJWIOKGNPMPGWYTNFMOW99999"]
-
-puts iota.api.getTransactionsObjects(hashes)[1]
